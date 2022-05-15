57ead3047174a4d988fcec95cf6741f350d18186
Edwin Jorge <ed.jorge1122@gmail.com>
2022-05-15 18:40:59 -0500

    ADD: todo model structure

---
+++ models/Todo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TodoSchema = new Schema({
+  info: {
+    type: String,
+    required: true
+  },
+  done: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+const Todo = mongoose.model('Todo', TodoSchema)
+
+module.exports = Todo
